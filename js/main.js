f298bffa72bd66d84ac0f1a0a3bb6bdfdde133c7
TatyanaTropkina <tropkina.tatyana@gmail.com>
2021-07-08 16:41:57 +0300

    fix animation

--- js/main.js
+++ js/main.js
@@ -1,31 +1,114 @@
+	
+$(function(){
+$(document).ready(function () {
+			let $preloader = $('.preloader'),
+			$loader = $preloader.find('.preloader__loader');
+			$loader.fadeOut();
+			$preloader.delay(250).fadeOut(200);
+	});
+});
+
 $(function () {
-	$('.menu__btn').on('click', function () {
-		$('.menu__list').toggleClass('open');
+	let menu = $('.menu__list');
+	let menuBtn = $('.menu__btn');
+	let menuLink = $('.menu__list-link');
+	let header = $('.header');
+
+	$(menuBtn).on('click', function () {
+		$(menu).toggleClass('open');
 		$(this).toggleClass('open');
-		$('.header').toggleClass('gradient');
+		$(header).toggleClass('gradient');
 	});
-	$(window).scroll(function () {
-		$('.menu__list').removeClass('open');
-		$('.header').removeClass('gradient');
-		$('.menu__btn').removeClass('open');
+	$(menuLink).on('click', function(){
+		$(menu).removeClass('open');
+		$(header).removeClass('gradient');
+		$(menuBtn).toggleClass('open');
 
-	});
-	let btn = $(".menu__btn");
-	let menu = $(".menu__list");
-
+	})
 	$(document).mouseup(function (e) {
-		if (!btn.is(e.target) && btn.has(e.target).length === 0 &&
+		if (!menuBtn.is(e.target) && menuBtn.has(e.target).length === 0 &&
 			!menu.is(e.target) && menu.has(e.target).length === 0
 		) {
 			menu.removeClass('open');
-			$('.header').removeClass('gradient');
-			$('.menu__btn.open').removeClass('open');
+			$(header).removeClass('gradient');
+			$(menuBtn).removeClass('open');
 		}
 	});
+	$(window).scroll(function () {
+		let scrollTop = $(window).scrollTop();
+		if (scrollTop > 500) {
+            $(menuBtn).removeClass('open');
+            $(menu).removeClass('open');
+			$(header).removeClass('gradient');
+        }
+    });
+});
+$(function (){
+	$(document).ready(function(){
+		
+		$('.header__content-img').addClass('animate__animated animate__fadeInUp')
+
+		$('.adapt__title-left').addClass('animate__animated animate__fadeInRightBig wow')
+		$('.adapt__title-left').attr('data-wow-offset', '300')
+		$('.adapt__title-right').addClass('animate__animated animate__fadeInLeftBig wow')
+		$('.adapt__title-right').attr('data-wow-offset', '100')
+		
+		$('.gallery__title-img').addClass('animate__animated animate__fadeInLeftBig wow')
+		$('.gallery__title-img').attr('data-wow-offset', '100');
+
+		$('.drink__img').addClass('animate__animated animate__zoomIn wow');
+		$('.drink__img').attr({
+			"data-wow-offset": "350",
+			"data-wow-delay": "1s"
+		});
+
+	});
+	$(document).ready(function(){
+
+		if ($(window).width() < 769) {
+			$('.section__content').addClass('animate__animated animate__fadeInUp wow');
+			$('.section__content').attr('data-wow-offset' , '100');
+
+			$('.drink__list-name').addClass('animate__animated animate__fadeInRightBig wow');
+			$('.drink__list-name').attr('data-wow-offset' , '200');
+
+			$('.drink__list-descr').addClass('animate__animated animate__fadeInLeftBig wow');
+			$('.drink__list-descr').attr('data-wow-offset' , '200');
+	
+			$('.blog__item').addClass('animate__animated animate__fadeInUp wow')
+
+		}else{
+			$('.section__content').addClass('animate__animated animate__fadeInRight wow');
+			$('.drink__list').addClass('animate__animated animate__fadeInRightBig wow');
+			$('.drink__list').attr('data-wow-offset' , '280');
+			$('.blog__item-box').addClass('animate__animated animate__fadeInUp wow')
+		}
+	})
+});
+$(function () {
+	
+	let drinkBg = $('.drink__img-bg')
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > drinkBg.offset().top - 450) {
+			drinkBg.addClass('anim');
+		}
+	});
+	let blogBtn = $('.blog__btn');
+	let blogForm = $('.blog__form');
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > blogBtn.offset().top - 400) {
+			blogBtn.addClass('anim');
+		
+			blogForm.css('opacity', '1');
+		}
+	});
+});
+$(function (){
 	$('.benefits-slider').slick({
 		slidesToShow: 3,
 		arrows: false,
-		// autoplay: true,
+		autoplay: true,
 		autoplaySpeed: 2500,
 		responsive: [
 			{
@@ -44,14 +127,11 @@ $(function () {
 		]
 	});
 
-	// $('.blog__btn').on('click', function () {
-	// 	$('.blog__form').toggleClass('show');
-	// });
 	$('.gallery-slider').slick({
 		slidesToShow: 4,
 		arrows: false,
-		// autoplay: true,
-		// autoplaySpeed: 2500,
+		autoplay: true,
+		autoplaySpeed: 2500,
 		swipe: true,
 		responsive: [{
 				breakpoint: 1101,
@@ -76,116 +156,6 @@ $(function () {
 		]
 
 	});
-	var reviews = $('.reviews');
-
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > reviews.offset().top - 200) {
-			reviews.addClass('anim');
-		}
-	});
-
-	let drinkItem = $('.drink__list-item');
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > drinkItem.offset().top - 50) {
-			drinkItem.addClass('anim');
-		}
-	});
-	let drinkBg = $('.drink__img-bg')
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > drinkBg.offset().top - 450) {
-			drinkBg.addClass('anim');
-		}
-	});
-	let blogBtn = $('.blog__btn');
-		let blogForm = $('.blog__form');
-		// blogForm.hide();
-
-
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > blogBtn.offset().top - 400) {
-			blogBtn.addClass('anim');
-		
-			blogForm.css('opacity', '1');
-		}
-	});
-	// let blogForm = $('.blog__form')
-	// $(window).scroll(function () {
-	// 	if ($(this).scrollTop() > blogForm.offset().top - 250) {
-	// 		blogForm.addClass('anim');
-	// 	}
-	// });
-
-	$(document).ready(function(){
-		
-		$('.adapt__title-left').addClass('animate__animated animate__fadeInRightBig wow')
-		$('.adapt__title-left').attr('data-wow-offset', '300')
-		$('.adapt__title-right').addClass('animate__animated animate__fadeInLeftBig wow')
-		$('.adapt__title-right').attr('data-wow-offset', '100')
-		
-		$('.gallery__title-img').addClass('animate__animated animate__fadeInLeftBig wow')
-		$('.gallery__title-img').attr('data-wow-offset', '100');
-
-		$('.drink__img').addClass('animate__animated animate__zoomIn wow');
-		$('.drink__img').attr({
-			"data-wow-offset": "350",
-			"data-wow-delay": "1s"
-		});
-
-	})
-	$(document).ready(function(){
-
-		if ($(window).width() < 769) {
-			$('.blog__item-box').removeClass('animate__animated animate__fadeInUp wow')
-			$('.blog__item').addClass('animate__animated animate__fadeInUp wow')
-
-			$('.drink__list').removeClass('animate__animated animate__fadeInRightBig wow');
-			$('.drink__list').removeAttr('data-wow-offset' , '280');
-	
-			$('.drink__list-name').addClass('animate__animated animate__fadeInRightBig wow');
-			$('.drink__list-descr').addClass('animate__animated animate__fadeInLeftBig wow');
-	
-	
-			$('.drink__list-name').attr('data-wow-offset' , '200');
-			$('.drink__list-descr').attr('data-wow-offset' , '200');
-	
-			$('.section__content').addClass('animate__animated animate__fadeInUp wow');
-			$('.section__content').attr('data-wow-offset' , '100');
-		}else{
-			$('.section__content').addClass('animate__animated animate__fadeInRight wow');
-
-		}
-	})
-	// if ($(window).width() < 767) {
-
-	// 	$('.drink__list-item:nth-child(even)').addClass('animate__animated animate__fadeInLeftBig wow');
-	// 	$('.section__content').removeClass('animate__animated animate__fadeInLeftBig wow animated');
-	// 	$('.section__content').removeClass(' animated');
-	// 	$('.section__content').removeAttr('data-wow-offset');
-	// 	$('.adapt__title-left, .adapt__title-right').attr('data-wow-offset' , '100');
-	// 	$('.gallery__title').attr('data-wow-offset' , '100');
-	// 	$('.works__energy-anim , works__stressed-anim').removeAttr('data-wow-offset')
-	// }
-});
-$(function () {
-	// $('.header__content-img').addClass('animate__animated animate__fadeInUpBig');
-
-})
-$(function () {
-
-
-	
-	  
-	//  else {
-
-	//  }
-	//  if ($(window).width() > 768) {
-	// 	$('.drink__content').toggleClass('animate__animated animate__fadeInUp wow');
-	// 	// $('.drink__content').attr('data-wow-offset' , '300');
-	//  }
-	 
-
-	
-
 });
 $(function () {
 	$(document).ready(function () {
